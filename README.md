34a78f94e0162554f695ec70532425704adc2715
Tkachuk Ilya <tim@ip-176-193-131-104.bb.netbynet.ru>
2015-05-24 01:09:22 +0300

    it works

---
+++ README.md
@@ -0,0 +1,22 @@
+Csvex
+=====
+
+Note, you have to give uniform list of maps (structs). Example of usage :
+
+```
+res = Csvex.encode([%{key1: 123, key2: 321}, %{key1: "qwe\n\r,", key2: "qwd,,"}])
+File.write!("./foo.csv", res)
+```
+
+or
+
+```
+res = Csvex.encode([%{key1: 123, key2: 321}, %{key1: "qwe\n\r,", key2: "qwd,,"}], %{separator: ";", str_separator: "\r\n"})
+File.write!("./foo.csv", res)
+```
+
+Next you can convert your file to *.xls file this way
+
+```
+csv2xls ./foo.csv
+```
---
+++ lib/csvex.ex
@@ -0,0 +1,55 @@
+defmodule Csvex do
+  use Application
+  use Silverb
+
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      # Define workers and child supervisors to be supervised
+      # worker(Csvex.Worker, [arg1, arg2, arg3])
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Csvex.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  defp default_opt(:separator), do: ";"
+  defp default_opt(:str_separator), do: "\n"
+  
+  def encode(lst, opts \\ %{})
+  def encode([], _), do: ""
+  def encode([first|_] = lst, opts) when is_list(lst) do
+  	required_keys = Map.keys(first)
+	case Enum.filter(lst, &(not(required_keys == Map.keys(&1)))) do
+		[] ->	Enum.reduce([:separator, :str_separator], opts, 
+	  			fn(k, opts) ->
+					case Map.get(opts, k) do
+						nil -> Map.put(opts, k, default_opt(k))
+						bin when is_binary(bin) -> opts
+					end
+	  			end) 
+				|> encode_proc(required_keys, lst)
+		err -> 	raise "#{__MODULE__} not uniform map(s) #{inspect err}"
+	end
+  end
+
+  defp encode_proc(opts = %{separator: sep}, required_keys, lst) do 
+	[Stream.map(required_keys, &(CSV.Encode.encode(&1, separator: sep))) |> Enum.join(sep)]
+	|> encode_values(lst, required_keys, opts)
+  end
+
+
+  defp encode_values(reslst, [], _, %{str_separator: str_sep}), do: (Enum.reverse(reslst) |> Enum.join(str_sep))<>str_sep
+  defp encode_values(reslst, [el|rest], required_keys, opts = %{separator: sep}) do
+  	[Stream.map(required_keys, &(Map.get(el, &1) |> CSV.Encode.encode(separator: sep))) |> Enum.join(sep) |reslst]
+  	|> encode_values(rest, required_keys, opts)
+  end
+
+
+
+end
